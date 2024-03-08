2f06a746d35a057430949d61954d248021ab3e1e
JunTsai <Cchoyjun@gmail.com>
2024-03-08 16:13:13 +0800

    feat: update previous article

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import recoTheme from "vuepress-theme-reco";
 
 export default defineUserConfig({
-  title: "脑瓜像个小风扇，转啊转啊...",
+  title: "脑瓜转啊转啊...",
   description: "Facing the Ocean, Spring and Blossom",
   base: '/blog.github.io/',
   theme: recoTheme({
